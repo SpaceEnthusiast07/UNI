941724af9c980f9c0369810d69b9b5250b0036cf
Cameron <chrussellpredator07@gmail.com>
2025-11-01 15:39:00 +0000

    Finished the print_plane function for CW1 Q1

--- ECM1400 - Programming/Coursework/CW1/printPlaneTest.py
+++ ECM1400 - Programming/Coursework/CW1/printPlaneTest.py
@@ -44,21 +44,26 @@ plane = [["empty","empty","empty","empty","empty","empty"],
 
 
 print_nose()
+# Initialise the row string
 rowString = "| "
+# Loop through each row of the plane
 for i in range(len(plane)):
 		# If on row 11 (index 10), print exit row
 		if i == 10:
 			print("|" + " "*(len(rowString) - 2) + "|")
 		
+		# Reset row string
 		rowString = "| "
+		# Loop through each seat in the current row
 		for j in range(len(plane[i])):
-			# If on seat 4 (index 3), add "  " to rowString
+			# If on seat 4 (index 3), add "  " to rowString for isle
 			if j == 3:
 				rowString += "  "
 			
 			# If current seat empty, add "U " to rowString
 			if plane[i][j] == "empty":
 				rowString += "U "
+			# If current seat is booked, add "# " to rowString
 			elif plane[i][j] == "booked":
 				rowString += "# "
 			
