de7bad31c123cbcbb4581befefb6471bbb256c98
Cameron <chrussellpredator07@gmail.com>
2025-11-03 20:05:57 +0000

    Finalised the print_plane() function for Q1

--- ECM1400 - Programming/Coursework/CW1/printPlaneTest.py
+++ ECM1400 - Programming/Coursework/CW1/printPlaneTest.py
@@ -1,24 +1,24 @@
 
 def print_nose():
-	print(" "*7 + "/\\" + " "*7)
-	print(" "*6 + "/  \\" + " "*6)
-	print(" "*5 + "/    \\" + " "*5)
-	print(" "*4 + "/      \\" + " "*4)
-	print(" "*3 + "/        \\" + " "*3)
-	print(" "*2 + "/          \\" + " "*2)
-	print(" "*1 + "/            \\" + " "*1)
-	print("/              \\")
+	print(" "*9 + "/\\" + " "*7)
+	print(" "*8 + "/  \\" + " "*6)
+	print(" "*7 + "/    \\" + " "*5)
+	print(" "*6 + "/      \\" + " "*4)
+	print(" "*5 + "/        \\" + " "*3)
+	print(" "*4 + "/          \\" + " "*2)
+	print(" "*3 + "/            \\" + " "*1)
+	print("  /              \\")
 
 
 def print_tail():
-	print("\\              /")
-	print(" "*1 + "\\            /" + " "*1)
-	print("  \\          /" + " "*2)
-	print(" / \\        / \\" + " "*3)
-	print("/   \\      /   \\" + " "*4)
-	print("-----\\    /-----" + " "*5)
-	print(" "*6 + "\\  /" + " "*6)
-	print(" "*7 + "\\/" + " "*7)
+	print("  \\              /")
+	print(" "*3 + "\\            /" + " "*1)
+	print("    \\          /" + " "*2)
+	print("   / \\        / \\" + " "*3)
+	print("  /   \\      /   \\" + " "*4)
+	print("  -----\\    /-----" + " "*5)
+	print(" "*8 + "\\  /" + " "*6)
+	print(" "*9 + "\\/" + " "*7)
 
 
 plane = [["empty","empty","empty","empty","empty","empty"],
@@ -41,19 +41,21 @@ plane = [["empty","empty","empty","empty","empty","empty"],
 		 ["empty","empty","empty","empty","empty","empty"]]
 
 
-
+letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 print_nose()
+# Print the column numbers
+print("    1 2 3   4 5 6")
 # Initialise the row string
 rowString = "| "
 # Loop through each row of the plane
 for i in range(len(plane)):
 		# If on row 11 (index 10), print exit row
 		if i == 10:
-			print("|" + " "*(len(rowString) - 2) + "|")
+			print("  ¦" + " "*(len(rowString) - 4) + "¦")
 		
 		# Reset row string
-		rowString = "| "
+		rowString = letters[i] + " | "
 		# Loop through each seat in the current row
 		for j in range(len(plane[i])):
 			# If on seat 4 (index 3), add "  " to rowString for isle
@@ -73,4 +75,7 @@ for i in range(len(plane)):
 		# Output the current row
 		print(rowString)
 
+
+
+# Print the plaen's tail
 print_tail()
