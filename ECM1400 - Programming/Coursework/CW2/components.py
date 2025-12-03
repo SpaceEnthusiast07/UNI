2bcf36a4bfeb5f7201bd2daf0aca36862c65077d
Cameron <chrussellpredator07@gmail.com>
2025-12-03 18:50:00 +0000

    Finally removed all errors from components.py in programming cw2

--- ECM1400 - Programming/Coursework/CW2/components.py
+++ ECM1400 - Programming/Coursework/CW2/components.py
@@ -56,14 +56,11 @@ def print_board(board):
 
 
 # Function that determines if a move is legal or not
-def legal_move(colour, coordinate, board):
+def legal_move(colour, coordinate, board, modifyBoard):
     # To access a cell in a 2D array, the row index is provided before the column index
     # This means that the y-axis is provided before the x-axis
     # Wrong: board[xCoord, yCoord] = board[cellToCheck[0], cellToCheck[1]]
     # Right: board[yCoord, xCoord] = board[cellToCheck[1], cellToCheck[0]]
-
-    # First, lets check whether a counter is already present at this location
-    if (board[coordinate[1], coordinate[0]] != "None "): return False
 
     # Calculate the size of the board
     size = len(board[0])
@@ -73,6 +70,9 @@ def legal_move(colour, coordinate, board):
     coordinate = np.array(coordinate) - 1
     # Initialise the legalDirection, representing if there exists a direction that is legal
     legalDirection = False
+
+    # First, lets check whether a counter is already present at this location
+    if (board[coordinate[1], coordinate[0]] != "None "): return False
     
     # Loop through each direction
     for direction in directions:
@@ -84,20 +84,22 @@ def legal_move(colour, coordinate, board):
             # Check if the cell contains "None ", if not, a legal move is possible along this direction
             if (board[cellToCheck[1], cellToCheck[0]] != "None "):
                 # Analsye the first cell in this direction
-                result = analyse_cell(colour, cellToCheck, board, direction, size)
-                # Check if this direction has resulted in an outflank
-                if (result): 
+                result = analyse_cell(colour, cellToCheck, board, direction, size, modifyBoard)
+                # Check if this direction has resulted in an outflank, and we are allowed to modify the board
+                if (result and modifyBoard): 
                     legalDirection = True
                     board[coordinate[1], coordinate[0]] = colour
+                # If we are not allowed to modify the board, but the direction is legal, set legalDirection to true
+                elif (result == True and modifyBoard == False): legalDirection = True
     
     # Check if any direction has resulted in a legal move
     return legalDirection
 
 
 # Function to analyse a cell to determine if the player can outflank the other
-def analyse_cell(colour, cellToCheck, board, direction, size):
+def analyse_cell(colour, cellToCheck, board, direction, size, modifyBoard):
     # Base Case: Check if we are outside the boundaries of the board
-    if (cellToCheck[0] < 0 or cellToCheck[0] > size or cellToCheck[1] < 0 or cellToCheck[1] > size):
+    if (cellToCheck[0] < 0 or cellToCheck[0] >= size or cellToCheck[1] < 0 or cellToCheck[1] >= size):
         # Since we have gone outside the boundaries of the board, this direction must not contain any other counters
         return False
     
@@ -111,12 +113,14 @@ def analyse_cell(colour, cellToCheck, board, direction, size):
     # Otherwise, we must have reached the other player's colour
     else:
         # Since this cell contains the other player's colour, analyse the next cell along the current direction
-        result = analyse_cell(colour, cellToCheck + direction, board, direction, size)
+        result = analyse_cell(colour, cellToCheck + direction, board, direction, size, modifyBoard)
         # If result is true, that means player has outflanked the other player along this direction
-        if (result == True):
+        if (result and modifyBoard):
             # Therefore, change this cell to the player's colour
             board[cellToCheck[1], cellToCheck[0]] = colour
             return True
+        # If we are not allowed to modify the board, but this direction is still legal, return true
+        elif (result == True and modifyBoard == False): return True
         else:
             # Either direction is empty or player cannot outflank the other player in this direction
             return False
