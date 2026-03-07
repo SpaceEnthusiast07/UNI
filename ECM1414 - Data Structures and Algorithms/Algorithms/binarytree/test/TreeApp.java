ea305b3c3285d779a069272e0ad47212f03d9061
Cameron <chrussellpredator07@gmail.com>
2026-03-07 19:14:34 +0000

    Adapted program to take in terminal arguments and add them to the tree.

--- ECM1414 - Data Structures and Algorithms/Algorithms/binarytree/test/TreeApp.java
+++ ECM1414 - Data Structures and Algorithms/Algorithms/binarytree/test/TreeApp.java
@@ -2,14 +2,23 @@ import tree.*;
 
 public class TreeApp {
     public static void main(String[] args) {
-        // Create a new tree
-        Tree tree = new Tree(null, 3);
+        // Check if the user has entered anything into the terminal
+        if (args.length == 0) {
+            System.out.println("ERROR: Please enter at least the root node for the tree.");
+            System.exit(0);
+        }
 
-        // Add to more nodes
-        tree.add(4);
-        tree.add(1);
+        // Add the root node to the tree
+        Tree tree = new Tree(Integer.parseInt(args[0]));
 
-        // Ouput the tree
+        // If the user has entered any more nodes, add them
+        if (args.length > 1) {
+            for (int i = 1; i < args.length; i++) {
+                tree.add(Integer.parseInt(args[i]));
+            }
+        }
+
+        // Output the tree
         System.out.println(tree);
     }
 }
